117106a6ca75be4038dea1e74a93b0c45f09abe4
GuitherezDika <guitherez.git@gmail.com>
2024-06-12 11:36:48 +0700

    create memory api

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -7,54 +7,62 @@ import { createPost, updatePost } from "../../actions/posts";
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
+        title: '', message: '', tags: '', selectedFile: ''
     });
     const post = useSelector(state => currentId ? state.posts?.find(p => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
-        if(post) setPostData(post);
+        if (post) setPostData(post);
     }, [post]);
 
     const handleSubmit = e => {
         e.preventDefault(); // ngehindari untuk refresh saat di klik
-        
-        if(currentId){
-            dispatch(updatePost(currentId, postData))
-        } else {
-            dispatch(createPost(postData))
-        }
 
-        clear()
+        if (currentId === 0) {
+            dispatch(createPost({ ...postData, name: user?.result?.name }))
+            clear()
+        } else {
+            dispatch(updatePost({ ...postData, name: user?.result?.name }))
+            clear()
+        }
     };
     const clear = () => {
         setCurrentId(null);
         setPostData({
-            creator: '', title: '', message: '', tags: '', selectedFile: ''
+            title: '', message: '', tags: '', selectedFile: ''
         })
     };
 
+    if (!user?.result?.name) {
+        return (
+            <Paper className={classes.paper}>
+                <Typography variant="h6" align="center">
+                    Please sign in to create your own memories and like other's memories.
+                </Typography>
+            </Paper>
+        )
+    }
+
     return (
         <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate 
-            className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate
+                className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6" >{currentId ? 'Editing' : 'Creating a Memory'}</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth 
-                    value={postData.creator}
-                    onChange={e => setPostData({ ...postData, creator: e.target.value})} 
-                />
+
                 <TextField name="title" variant="outlined" label="Title" fullWidth
                     value={postData.title}
-                    onChange={e => setPostData({ ...postData, title: e.target.value})} 
+                    onChange={e => setPostData({ ...postData, title: e.target.value })}
                 />
                 <TextField name="message" variant="outlined" label="Message" fullWidth
                     value={postData.message}
-                    onChange={e => setPostData({ ...postData, message: e.target.value})} 
+                    onChange={e => setPostData({ ...postData, message: e.target.value })}
                 />
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth
                     value={postData.tags}
-                    onChange={e => setPostData({ ...postData, tags: e.target.value.split(',')})} 
+                    onChange={e => setPostData({ ...postData, tags: e.target.value.split(',') })}
                 />
                 <div className={classes.fileInput}>
                     <FileBase
@@ -64,12 +72,12 @@ const Form = ({ currentId, setCurrentId }) => {
                     />
                 </div>
                 <Button
-                 className={classes.buttonSubmit}
-                 variant="contained" 
-                 color="primary" size="large"
-                 type="submit" fullWidth>Submit</Button>
+                    className={classes.buttonSubmit}
+                    variant="contained"
+                    color="primary" size="large"
+                    type="submit" fullWidth>Submit</Button>
                 <Button
-                    variant="contained" 
+                    variant="contained"
                     color="secondary" size="small"
                     onClick={clear} fullWidth>Clear</Button>
             </form>
@@ -78,4 +86,3 @@ const Form = ({ currentId, setCurrentId }) => {
 }
 
 export default Form
- 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,19 +6,19 @@ import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
 
 const Home = () => {
-    const [currentId, setCurrentId] = useState(null);
+    const [currentId, setCurrentId] = useState(0);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getPosts())
-    },[currentId, dispatch]);
+    }, [currentId, dispatch]);
 
     return (
         <Grow in>
             <Container>
                 <Grid container justify='space-between' alignItems='stretch'>
                     <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId}/>
+                        <Posts setCurrentId={setCurrentId} />
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
