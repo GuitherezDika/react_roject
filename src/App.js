12997f207060ada131bf33203612b0b3a09ec470
GuitherezDika <guitherez.git@gmail.com>
2024-06-13 23:48:15 +0700

    learn redirect - navigate to

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from "@material-ui/core";
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -9,18 +9,22 @@ import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
     const user = JSON.parse(localStorage.getItem('profile'));
-
+    console.log(user);
     return (
         <GoogleOAuthProvider clientId='720796861195-0kfgqf7340agfivb86f83qoe6ee1dd1a.apps.googleusercontent.com'>
             <BrowserRouter>
                 <Container maxwidth="lg">
                     <Navbar />
                     <Routes>
-                        <Route path='/' element={<Home />} />
+                        <Route path='/' element={<Navigate to="/posts" />}
+                        // Navigate to == Fungsi Redirect ke halaman Posts => route di url => "/posts"
+                        />
+
                         <Route path='/posts' element={<Home />} />
                         <Route path='/posts/search' element={<Home />} />
                         <Route path='/posts/:id' element={<PostDetails />} />
-                        <Route path='/auth' element={!user ? <Auth /> : <Home />} />
+                        <Route path='/auth' element={!user ? <Auth /> : <Navigate to="/posts" />} />
+                        {/* <Route path='/auth' element={!user ? <Auth /> : <Home />} /> */}
                     </Routes>
                 </Container>
             </BrowserRouter>
