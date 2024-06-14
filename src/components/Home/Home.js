21a86ff808acf29dc449eec3f8a8abb7d22e7b01
GuitherezDika <guitherez.git@gmail.com>
2024-06-14 09:29:52 +0700

    set ui and api for search component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,88 @@
-import { Container, Grid, Grow, Paper } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import { Container, Grid, Grow, Paper, AppBar, TextField, Button, Chip } from "@material-ui/core";
 import Posts from "../Posts/Posts";
 import Form from "../Forms/Form";
 import Pagination from '../Pagination';
 import { useDispatch } from "react-redux";
-import { getPosts } from "../../actions/posts";
-import { useLocation } from "react-router-dom";
+import { getPosts, getPostsBySearch } from "../../actions/posts";
+import { useNavigate, useLocation } from "react-router-dom";
+import ChipInput from 'material-ui-chip-input';
+
 import useStyles from './styles'
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search)
+}
+
 const Home = () => {
-    const [currentId, setCurrentId] = useState(0);
     const dispatch = useDispatch();
     const location = useLocation();
     const classes = useStyles();
+    const query = useQuery();
+    const history = useNavigate();
+
+    const [currentId, setCurrentId] = useState(0);
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
+
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+
+    const handleKeyPress = e => {
+        console.log(e.keyCode);
+        if (e.keyCode === 13) {
+            searchPost()
+        }
+    }
+
+    const handleAdd = tag => setTags([...tags, tag])
+    const handleDelete = tagToDelete => setTags(tags.filter(tag => tag !== tagToDelete));
+    const searchPost = () => {
+        if (search.trim()) {// 
+            // trim = penghilangan spasi di awal dan akhir
+            dispatch(getPostsBySearch({ search, tags: tags.join(',') }))
+        } else {
+            // search length = 0
+            history('/')
+        }
+    };
+
     useEffect(() => {
         dispatch(getPosts())
     }, [currentId, dispatch, location]);
 
     return (
         <Grow in>
-            <Container>
-                <Grid container justify='space-between' alignItems='stretch'>
-                    <Grid item xs={12} sm={7}>
+            <Container maxWidth={'xl'}>
+                <Grid container justify='space-between' alignItems='stretch' spacing={3} className={classes.gridContainer}>
+                    <Grid item xs={12} sm={6} md={9}>
                         <Posts setCurrentId={setCurrentId} />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                            <TextField
+                                name="search"
+                                variant="outlined"
+                                label="Search Memories"
+                                onKeyPress={handleKeyPress}
+                                fullWidth
+                                value={search}
+                                onChange={e => setSearch(e.target.value)}
+                            />
+
+                            <ChipInput
+                                style={{ margin: "10px 0" }}
+                                value={tags}
+                                onAdd={handleAdd}
+                                onDelete={handleDelete}
+                                label={"Search tags"}
+                                variant="outlined"
+                                newChipKeys={['Enter']}
+                            />
+                            <Button onClick={searchPost} className={classes.searchButton} variant="contained" color={'primary'}>
+                                Search
+                            </Button>
+                        </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                         <Paper className={classes.pagination} elevation={6}>
                             <Pagination />
